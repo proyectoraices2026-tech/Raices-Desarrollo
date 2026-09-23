6f581eb081cf57ae48b1e5c27b998ef52dd872d5
DaisyMPerez <daisyperher19@gmail.com>
2026-09-23 09:34:51 -0600

    no

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,7 @@ import Catalog from './pages/Catalog';
 import AdminProducts from './pages/AdminProducts';
 import AdminRequests from './pages/AdminRequests';
 
-//aaaaaaaaaaaaaaaaa si
+//aaa
 import MyPlants from './pages/MyPlants';
 import AboutUs from './pages/AboutUs';
 import Cart from './pages/Cart';
